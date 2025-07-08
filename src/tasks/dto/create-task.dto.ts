c500f0922a608eaae81e54a8349b20c0164740d6
EBRARR15 <ebraraltunkynk@gmail.com>
2025-07-08 16:02:32 +0300

    class validator

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,6 +1,12 @@
+import { IsNotEmpty} from "class-validator";
+
+
 export class CreateTaskDto{
 
+    @IsNotEmpty()
     title: string;
+
+    @IsNotEmpty()
     description: string;
     // status: TaskStatus; // Uncomment if you want to include status in the DTO
 
