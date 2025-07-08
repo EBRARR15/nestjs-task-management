8c0e1c0723637318baa1814c0fd02171f4e5400a
EBRARR15 <ebraraltunkynk@gmail.com>
2025-07-08 11:39:00 +0300

    get task by id

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -12,6 +12,11 @@ constructor( private tasksService: TasksService) {}
         return this.tasksService.getAllTasks(); 
     }
 
+
+    @Get('/:id')
+    getTaskById(@Param('id') id: string): Task {
+        return this.tasksService.getTaskById(id);
+    }
 
     @Post()
     createTask(
