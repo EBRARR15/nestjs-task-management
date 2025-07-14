c983d4fb3911b19543250e0d324c2024a1c97201
EBRARR15 <ebraraltunkynk@gmail.com>
2025-07-14 13:16:25 +0300

    connect database pg

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TasksModule],
-
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // tüm uygulamada erişilebilir
+    }),
+    TasksModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true, // Production'da false yap!
+      }),
+    }),
+  ],
 })
 export class AppModule {}
