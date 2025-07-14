f3a22d871d98434008a7e68df83a67096074274c
EBRARR15 <ebraraltunkynk@gmail.com>
2025-07-14 14:02:02 +0300

    feat: implement TasksModule and TasksRepository for task management

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -2,7 +2,14 @@ import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TasksRepository } from './tasks.repository';
+
 @Module({
+
+  imports: [
+    TypeOrmModule.forFeature([TasksRepository]) // TypeOrmModule ile Task entity'sini ekliyoruz
+  ],
   controllers: [TasksController],
   providers: [TasksService]
 })
